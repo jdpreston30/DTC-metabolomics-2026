81b8d2c89e012c869c48b618c99199d6be3e390b
Josh Preston <jdpreston30@gmail.com>
2026-01-12 14:01:06 -0500

    updated 03

--- R/Scripts/03_annotated.R
+++ R/Scripts/03_annotated.R
@@ -1,17 +1,16 @@
 #* 3: Annotated Plots
 #+ 3.1: T-tests for all metabolomic features against PGD status
-#- 3.1.1: Transform feature table and run T-test function
+#- 3.1.1: Transform feature table
 TFT_annot_transformed <- TFT_annot |> 
   mutate(stage_bin = ifelse(stage_bin == "Early", "Stage I/II", "Stage III/IV"))
-
+#- 3.1.2: Run targeted t-tests
 annot_results <- run_targeted_ttests(
   feature_table = TFT_annot_transformed,
   grouping_var = "stage_bin",
   fc_ref_group = "Stage I/II"
 )
-#- 3.1.2: Clean up annotated results
+#- 3.1.3: Clean up annotated results
 annot_results_clean <- annot_results |>
-  filter(low_detect_likely != "Y") |>
   select(-low_detect_likely) |>
   left_join(TFT_annot_key |> select(feature = Feature, `Identified Name`, Isomer), by = "feature") |>
   arrange(p_value) |>
@@ -25,7 +24,6 @@ annot_results_clean <- annot_results |>
   )) |>
   mutate(`Identified Name` = ifelse(`Identified Name` == "(6Z,9Z,12Z)-Octadecatrienoic acid", "γ-Linolenic Acid", `Identified Name`)) |>
   mutate(`Identified Name` = ifelse(`Identified Name` == "S-Adenosyl-L-homocysteine", "SAH", `Identified Name`))
-
 #+ 3.2: Source the plotting function
 source("R/Utilities/Visualization/plot_stage_targeted.R")
 #+ 3.3: Create individual feature plots
